769920017f92f86301373810cec079f046a83568
“Priti” <pritihirpar256@gmail.com>
2024-06-01 16:02:18 +0530

    01/06/2024

--- Tailwind CSS/tailwind.config.js
+++ Tailwind CSS/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  content: ["./404/**/*.{html,js}"], 
-  content: ["./Plant/**/*.{html,js}"], 
+  content: ["./1.404error/**/*.{html,js}"], 
+  content: ["./2.Plant/**/*.{html,js}"], 
+  content: ["./3.Breadcrumbs/**/*.{html,js}"], 
+  content: ["./4.call-to-actio-section/**/*.{html,js}"], 
+  content: ["./5.card-grid/**/*.{html,js}"], 
   darkMode: 'selector',
   theme: {
     screens:{
