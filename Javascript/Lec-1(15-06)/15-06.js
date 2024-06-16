bce20f63cc3fe67bdf43c88479ae0279f281d529
“Priti” <pritihirpar256@gmail.com>
2024-06-16 16:05:58 +0530

    16/06/2024

---
+++ Javascript/Lec-1(15-06)/15-06.js
@@ -0,0 +1,100 @@
+/* Javascript Introduction */
+
+//comments  (single line)
+/* add comments */  /* (multi line) */
+
+/* Javascript is Set of Instruction */
+// Sets of Instruction = Statements
+
+// Version Defination
+
+/*
+[2015-2016]
+Es5
+Es6
+*/
+
+/* Variable Declaration var Keyword */
+
+// var x = 10;  //Javascript Statement
+
+//console.log()
+//Document.write
+//Console.warn()
+//alert()
+//document.getElementById("demo").innerHTML = content;
+
+/*
+
+chrome : - V8 Engine
+Mozilla Firefox :- Spider Monkey
+
+*/
+
+{
+    // console.log(x);
+    // document.write(x);
+    // console.warn(x);
+    // alert(x);
+    // document.getElementById("demo").innerHTML = x;
+    // DOM:-Document Object MOdel Method
+}
+
+// Scope : {}
+
+/* var Variables */
+
+{
+    // var x = 10;
+    // var y = 20;
+    // var z = x + y;
+    // console.log(z);
+}
+
+/* var variable Global variable */
+
+// var x;
+
+// console.log(x);
+
+// x = 10;
+
+// {
+//     console.log(x);
+// }
+
+// var x = 20;
+
+// {
+//     console.log(x);
+// }
+
+/* Javascript CaseSensitive Language */
+
+// var box = 10;
+// var Box = 20;
+
+// console.log(box);
+// console.log(Box);
+
+// var element = "skillqode" 
+
+// var _element = "skillqode" 
+
+// var element_10 = "skillqode"
+
+// var element_ = "skillqode"
+
+// var 10_element = "skillqode"   //not allowed
+
+// var 20 = "skillqode"    //not allowed
+
+// var $element = "skillqode"
+
+// console.log(element);
+// console.log(_element);
+// console.log(element_10);
+// console.log(element_);
+// console.log(10_element);
+// console.log(20);
+// console.log($element);
