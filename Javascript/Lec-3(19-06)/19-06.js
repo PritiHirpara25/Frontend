5373f7883b56ff633e264205453e71b328bfc0ca
“Priti” <pritihirpar256@gmail.com>
2024-06-25 18:35:42 +0530

    25/06/2024

--- Javascript/Lec-3(19-06)/19-06.js
+++ Javascript/Lec-3(19-06)/19-06.js
@@ -1,1 +1,313 @@
-/*  */
+/* Javascript  String */
+
+// let string1 ="This is String Data"
+// let string2 = 'this is string data'
+// let string3 = `this is string data`
+// // let string4 ="This is "String" Data"
+// let string5 = "This is \"string\" data"
+// // let string6 = 'this is 'string' data'
+// let string7 = 'this is \'string\' data'
+// // let string8 = `this is `string` data`
+// let string9 = `this is \`string\` data`
+
+// console.log(string1)
+// console.log(string2)
+// console.log(string3)
+// // console.log(string4)
+// console.log(string5)
+// // console.log(string6)
+// console.log(string7)
+// // console.log(string8)
+// console.log(string9)
+
+// //typeof operator (typeof is inbuilt function)
+
+// console.log(typeof string1)
+// console.log(typeof string2)
+// console.log(typeof string3)
+// console.log(typeof string5)
+// console.log(typeof string7)
+// console.log(typeof string9)
+
+/* Javascript String Variables */
+
+// let UserName = "Ronak"
+
+// let string1 = "${UserName} is a very clever boy!"      //(this will not  show)
+// let string2 = '${UserName} is a very clever boy!'      //(this will not show)
+// let string3 = `${UserName} is a very clever boy!`      //(this will show)
+
+// console.log(string1)
+// console.log(string2)
+// console.log(string3)
+
+/* string And number */
+
+// {
+//     let str1 = "10"    //(string)
+//     let str2 = "20"    //(string)
+    
+//     let str3 = str1 + str2
+    
+//     console.log(str3)
+    
+//     console.log(typeof str3)
+// }
+
+// {
+//     let str1 = 10   //(number)
+//     let str2 = "20"   //(string)
+    
+//     let str3 = str1 + str2
+    
+//     console.log(str3)
+    
+//     console.log(typeof str3)    //(it convert number datatype to string datatype)
+// }
+
+// {
+//     let str1 = 10   //(number)
+//     let str2 = 20   //(number)
+    
+//     let str3 = str1 + str2
+    
+//     console.log(str3)
+    
+//     console.log(typeof str3)    
+// }
+
+// {
+//     let str1 = 10   //(number)
+//     let str2 = "20"   //(string)
+    
+//     let str3 = str1 + str2 + str2
+    
+//     console.log(str3)
+    
+//     console.log(typeof str3)    
+// }
+
+// {
+//     let str1 = 10   //(number)
+//     let str2 = "20"   //(string)
+    
+//     let str3 = str1 + str2 + str2 * str1
+//                // 10 + 20 + 200 = 1020200
+    
+//     console.log(str3)
+    
+//     console.log(typeof str3)    
+// }
+
+// {
+//     let str1 = 10   //(number)
+//     let str2 = "20"   //(string)
+    
+//     let str3 = str1 + str1 + str2 + str2 * str1
+//              // 10 + 10 + 20 + 20 * 10
+//              // 20 + 20 + 200
+//              //2020200
+    
+//     console.log(str3)
+    
+//     console.log(typeof str3)    
+// }
+
+/* Type Conversion */
+
+// let str = "100"
+// console.log(str);
+
+// console.log(typeof str)
+
+// let num=Number(str)  //convert string to number
+// console.log(typeof num)
+// console.log(num);
+
+// let str1 = String(num)    //convert number to string
+// console.log(typeof str1)
+// console.log(str1)
+
+// let bool1 = true
+// let bool2 = false
+
+// let num1 = Number(bool1)
+// console.log(typeof num1)
+// console.log(num1)   
+
+// let num2 = Number(bool2)
+// console.log(typeof num2)
+// console.log(num2)
+
+
+/* Javascript Opeartors */
+
+/* arithmetic operator */
+/* conditional operator */
+/* assignment operator */
+/* bitwise operator */
+/* logical operator */
+/* ternory opearator */
+
+/* arithmetic operator */
+
+/* Addition */
+
+// {
+//     let x = 10
+//     let y = 20
+//     let z = x + y
+
+//     console.log(z)
+// }
+
+/* substraction */
+
+// {
+//     let x = 10
+//     let y = 20
+//     let z = x - y
+    
+//     console.log(z)
+// }
+
+/* multiplication */
+
+// {
+//     let x = 10
+//     let y = 20
+//     let z = x * y
+    
+//     console.log(z)
+// }
+
+/* divison */
+
+// {
+//     let x = 10
+//     let y = 20
+//     let z = x / y
+    
+//     console.log(z)
+// }
+
+/* modulus */
+
+// {
+//     let x = 10
+//     let y = 20
+//     let z = x % y
+    
+//     console.log(z)
+// }
+
+/* Expontiation */
+
+// {
+//     let x = 4
+//     let y = 2      
+//     let z = x ** y   //4 x 4 = 16
+    
+//     console.log(z)
+// }
+
+
+/* conditional operator */
+
+/* Equal to */
+
+// {
+//     let x = 10
+//     let y = 20
+//     let z = x = y
+
+//     console.log(z)
+// }
+
+/* Equality operator */  /* compare of value */
+
+// {
+//     let x = 10
+//     let y = '20'
+//     let z = x == y
+
+//     console.log(z)
+// }
+
+// {
+//     let xx = true
+//     let yy = 'false'
+//     let zz = xx == yy
+
+//     console.log(zz)
+// }
+
+/* strict equality operator */   /* compare value and datatype */
+
+// {
+//     let x = 10
+//     let y = 10
+//     let z = x == y
+
+//     console.log(z)
+// }
+
+/* Not eqaul operator */
+
+// {
+//     let x = 10
+//     let y = 20
+//     let z = x != y
+
+//     console.log(z)
+// }
+
+/* Not strict eqaulity operator */
+
+// {
+//     let x = '10'
+//     let y = 10
+//     let z = x !== y
+
+//     console.log(z)
+// }
+
+/* Less than */
+
+// {
+//     let x = 10
+//     let y = 20
+//     let z = x < y
+
+//     console.log(z)
+// }
+
+/* Less than or eqaul to */
+
+// {
+//     let x = 20
+//     let y = 20
+//     let z = x <= y
+
+//     console.log(z)
+// }
+
+/* Grether than */
+
+// {
+//     let x = 20
+//     let y = 20
+//     let z = x > y
+
+//     console.log(z)
+// }
+
+/* Grether than or equal to*/
+
+// {
+//     let x = 20
+//     let y = 20
+//     let z = x >= y
+
+//     console.log(z)
+// }
---
+++ Javascript/Lec-5(24-06)/24-06.js
@@ -0,0 +1,95 @@
+/* Javascript Output Model */
+
+/*
+
+C/C++
+
+int 
+chrt
+float
+string
+double
+boolean
+number
+object
+array
+undefined
+null
+
+ */
+
+/* Datatypes / Primitive Datatypes And Non-Primitive Datatypes */
+
+/* Primitive Datatypes / Immutable */
+
+/*
+
+String 
+Number
+Boolean
+BigInt
+null
+undefined
+Symbol
+
+*/
+
+/* Primitive Datatypes / Mutable */
+
+/*
+
+array
+object
+
+*/
+
+// var greek = 'Skillqode Learning Institute!' //string Datatypes
+
+// console.log(greek);
+// console.warn(greek);
+// document.write(greek);
+// alert(greek);
+
+// DOM Document Object Model (html document file nodes handle)
+// document.getElementById('demo').innerHTML = greek;
+
+/* String Methods */
+/* String Prototypes */
+
+/* String Write Method */
+
+// {
+//     var names = "Rahul"
+
+//     var str1 = 'This is \'string\' "Method" ${names}!'
+//     var str2 = "This is \"string\" 'Method' ${names}!"
+//     var str3 = `'This' is "string" Method ${names}!`
+    
+//     console.log(str1)
+//     console.log(str2)
+//     console.log(str3)
+
+//     var str4 = new String('this is string Method') 
+//     console.log(str4);
+// }
+
+/* String Prototypes */
+
+/* String String.concat() Method */
+
+// The concat() method of string values concatenates the string argumnet to this string and returns a new string and returns a new string.
+
+// concat()
+// concat(str1)
+// concat(str1,str2)
+// concat(str1, str2, /*..., */ strN)
+
+// {
+//     let str = "This is First String!"
+//     let str2 = "This is Second String!"
+//     let str3 = "This is Third String!"
+
+//     let MultiStr = str3.concat(str2, str)
+
+//     console.log(MultiStr);
+// }
