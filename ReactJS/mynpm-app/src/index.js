04d0e2aebe5294fc1cade046d1378e829d936b94
“Priti” <pritihirpar256@gmail.com>
2024-08-12 11:51:13 +0530

    12-08-2024

--- ReactJS/mynpm-app/src/index.js
+++ ReactJS/mynpm-app/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CardThree } from './component/Card';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <CardThree />
   </React.StrictMode>
 );
 
