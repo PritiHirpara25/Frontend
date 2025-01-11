598fe06a2aff4aefe712a56cd20f4b1e12ada04b
PritiHirpara25 <pritihirpara256@gmail.com>
2025-01-11 20:16:36 +0530

    Focus input Task Completed

--- ReactJs(Revision)/PracticeTask/src/Focus.jsx
+++ ReactJs(Revision)/PracticeTask/src/Focus.jsx
@@ -1,22 +1,22 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef } from 'react'
 
 const Focus = () => {
 
-    // const [focus , setFocus] = useState(false);
 
-    const refbtn = useRef();
+    const btnref = useRef(null);
+    // console.log(inputref)
 
-    // const handleState = () => {
-    //     setFocus(true)
-    // }
+    const handleState = () => {
+      btnref.current.focus();
+  }
 
   return (
-    <div>
+    <div className='m-2'>
         <div>
-            <input type="text" className='border-2 border-red' />
+            <input ref={btnref} type="text" placeholder='click btn for Focus' className='bg-red-100 rounded-md p-2 border-2 border-red-400'/>
         </div>  
         <div>
-            {/* <button onClick={}>Click</button> */}
+            <button onClick={handleState} className='bg-red-200 p-1.5 m-2 border-2 border-black'>Click</button>
         </div>
     </div>
   )
