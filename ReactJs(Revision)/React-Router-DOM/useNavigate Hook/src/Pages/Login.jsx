4ded4a4cbc29bece7215dac1d35f847e668789ce
PritiHirpara25 <pritihirpara256@gmail.com>
2025-01-20 20:41:05 +0530

    20/01/2025

--- ReactJs(Revision)/React-Router-DOM/useNavigate Hook/src/Pages/Login.jsx
+++ ReactJs(Revision)/React-Router-DOM/useNavigate Hook/src/Pages/Login.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
 
     const navigate = useNavigate()
 
-    function redirect () {
-        navigate('/')
-    }
+    // function redirect () {
+    //     navigate('/')
+    // }
+
+    useEffect(() => {
+        setTimeout(() => {
+            navigate('/')
+        }, 5000)
+        return () => clearTimeout();
+    })
+    
+
   return (
     <div>
-        <button onClick={redirect}>Home</button>
+        {/* <button onClick={redirect}>Home</button> */}
     </div>
   )
 }
