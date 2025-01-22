4bab90ab8c038754a8b60773656a4bc154a714a7
PritiHirpara25 <pritihirpara256@gmail.com>
2025-01-22 07:16:03 +0530

    22/01/2025

---
+++ ReactJs(Revision)/TASK/Expense-Tracker-App/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AddExpense from './Components/AddExpense'
+
+function App() {
+
+  return (
+    <>
+      <AddExpense/>
+    </>
+  )
+}
+
+export default App
---
+++ ReactJs(Revision)/TASK/Expense-Tracker-App/src/Components/AddExpense.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const AddExpense = () => {
+  return (
+    <div>
+      <div>
+        <label htmlFor="">Expense:</label>
+        <input type="text" name="" id="" />
+      </div>
+      <div>
+        <label htmlFor="">Expense Amount:</label>
+        <input type="text" name="" id="" />
+      </div>
+      <div>
+        <label htmlFor="">Category:</label>
+        <input type="text" name="" id="" />
+      </div>
+    </div>
+  )
+}
+
+export default AddExpense
