48a916557d71e61a06aa9f5a1c098e70a17a0369
PritiHirpara25 <pritihirpara256@gmail.com>
2025-02-14 11:39:53 +0530

    ...

--- ReactJs(Revision)/Redux/Slice/src/Redux.jsx
+++ ReactJs(Revision)/Redux/Slice/src/Redux.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { increment } from './ProductSlice'
+import { increment , decrement } from './ProductSlice'
 
 const Redux = () => {
 
@@ -10,9 +10,9 @@ const Redux = () => {
 
     return (
         <div>
-        <p>{data}</p>
-        <button onClick={() => {dispatch(increment())}}>Increment</button>
-        <button onClick={() => {dispatch(decrement())}}>Decrement</button>
+        <p className='m-5 bg-red-300 w-fit p-2 rounded-full'>{data}</p>
+        <button className='p-2 bg-red-200 m-5 ' onClick={() => {dispatch(increment())}}>Increment</button>
+        <button className='p-2 bg-red-200 m-5 ' onClick={() => {dispatch(decrement())}}>Decrement</button>
         </div>
     )
 }
